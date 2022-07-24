773865ac52519a39f5a83bf97ca6c0dcec626589
peoxin <1610860309@qq.com>
2022-07-25 02:51:17 +0800

    Update voltoggle.sh

--- .dwm/voltoggle.sh
+++ .dwm/voltoggle.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # Edit them according to your devices
-vol_state=`amixer get Master | awk -F "[][]" '/Left:/ {print $6}'`
+vol_state=`amixer get Master | awk -F "[][]" '/Mono:/ {print $6}'`
 if [ ${vol_state} = "on" ]; then
 	amixer set Master off
 else
 	amixer set Master on
 	amixer set PCM on
+	amixer set Speaker on
+	amixer set Headphone on
 fi	
